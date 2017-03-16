e46f4b781c59958b2f74d1c34840acaaf79888d2
zhaoyanqing624 <77528570@qq.com>
2017-03-16 22:48:52 +0800

    2017.3.16

--- org.xjtusicd3.partner/src/main/java/org/xjtusicd3/parnter/spider/FAQSpider.java
+++ org.xjtusicd3.partner/src/main/java/org/xjtusicd3/parnter/spider/FAQSpider.java
@@ -1,65 +1,47 @@
 package org.xjtusicd3.parnter.spider;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.http.HttpServletResponse;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.pipeline.JsonFilePipeline;
 import us.codecraft.webmagic.processor.PageProcessor;
 
-@Controller
-public class FAQSpider implements PageProcessor{
-	private Site site = Site.me().setDomain("www.pm25.com");
-	@Override
-	public Site getSite() {
-		return site;
-	}
-	@Override
-	public void process(Page page) {
-		page.putField("number",page.getHtml().xpath("//div[@class='bi_aqiarea_top']/a/text()").get());
-        page.putField("evaluate",page.getHtml().xpath("//div[@class='bi_aqiarea_top']/p/span[1]/text()").get());
-	}
-	public static String ReadFile(String path){
-		File file = new File(path);
-		BufferedReader reader = null;
-		String laststr="";
-		try {
-			reader = new BufferedReader(new FileReader(file));  
-            String tempString = null;  
-            while ((tempString = reader.readLine()) != null) {  
-                laststr = laststr + tempString;  
-            }
-            reader.close();
-        }catch(IOException e){
-        	e.printStackTrace();
-		} finally {
-			if (reader != null) {  
-                try {  
-                    reader.close();  
-                } catch (IOException e1) {  
-                }  
-            } 
-		}
-		return laststr;
-		
-	}
-	//动态获取雾霾信息
-	@RequestMapping(value={"getWumai"},method={org.springframework.web.bind.annotation.RequestMethod.GET},produces="text/html;charset=UTF-8")
-	public void getWeather(HttpServletResponse response) throws IOException{
-		Spider.create(new FAQSpider()).addUrl("http://www.pm25.com/").addPipeline(new JsonFilePipeline("D:\\webmagic\\")).run();
-		String result = ReadFile("D:/webmagic/www.pm25.com/c70da8eaca83f9d002b616b5074d870d.json");
-		System.out.println(result);
-		PrintWriter out = response.getWriter();
-		out.write(result);
-		return;
-	}
-	
+public class FAQSpider implements PageProcessor {
+
+    // 部分一：抓取网站的相关配置，包括编码、抓取间隔、重试次数等
+    private Site site = Site.me().setRetryTimes(3).setSleepTime(1000);
+
+    @Override
+    // process是定制爬虫逻辑的核心接口，在这里编写抽取逻辑
+    public void process(Page page) {
+        // 部分二：定义如何抽取页面信息，并保存下来
+        page.putField("author", page.getUrl().regex("https://github\\.com/(\\w+)/.*").toString());
+        page.putField("name", page.getHtml().xpath("//h1[@class='entry-title public']/strong/a/text()").toString());
+        if (page.getResultItems().get("name") == null) {
+            //skip this page
+            page.setSkip(true);
+        }
+        page.putField("readme", page.getHtml().xpath("//div[@id='readme']/tidyText()"));
+
+        // 部分三：从页面发现后续的url地址来抓取
+        page.addTargetRequests(page.getHtml().links().regex("(https://github\\.com/[\\w\\-]+/[\\w\\-]+)").all());
+    }
+
+    @Override
+    public Site getSite() {
+        return site;
+    }
+
+    public static void main(String[] args) {
+
+        Spider.create(new FAQSpider())
+                //从"https://github.com/code4craft"开始抓
+                .addUrl("https://github.com/code4craft")
+                //开启5个线程抓取
+                .addPipeline(new JsonFilePipeline("D:\\webmagic\\"))
+                .thread(5)
+                //启动爬虫
+                .run();
+    }
 }
 
